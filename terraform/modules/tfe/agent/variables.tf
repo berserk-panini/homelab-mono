16996c99110a2db9556013c53436eae2c5af74fb
edgy-noodle <alexwavresz@gmail.com>
2025-01-26 13:00:28 +0100

    style(tf/tfe): var attr order

--- terraform/modules/tfe/agent/variables.tf
+++ terraform/modules/tfe/agent/variables.tf
@@ -1,22 +1,23 @@
 variable "pool_name" {
   description = "Name of the agent pool."
+  nullable    = false
   type        = string
 }
 
 variable "organization_scoped" {
+  default     = true
   description = "Whether the agent pool is scoped to all workspaces in the organization."
   type        = bool
-  default     = true
 }
 
 variable "allowed_workspace_ids" {
+  default     = []
   description = "IDs of the workspaces that are allowed to use the agent pool."
   type        = set(string)
-  default     = []
 }
 
 variable "agent_count" {
+  default     = 1
   description = "Number of agents to create."
   type        = number
-  default     = 1
 }
